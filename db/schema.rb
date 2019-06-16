3c89b8aa86d4d352cfbea1da6455bf2fa3b1249d
Chi Chen <chenchiapply@gmail.com>
2019-06-16 02:05:23 -0400

    [Twitter] add tweet model and migration file

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20190615025623) do
+ActiveRecord::Schema.define(version: 20190616023722) do
+
+  create_table "tweets", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
+    t.integer  "user_id"
+    t.string   "text"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_tweets_on_user_id", using: :btree
+  end
 
   create_table "users", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=latin1" do |t|
     t.datetime "created_at",                          null: false
@@ -24,4 +32,5 @@ ActiveRecord::Schema.define(version: 20190615025623) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
   end
 
+  add_foreign_key "tweets", "users"
 end
