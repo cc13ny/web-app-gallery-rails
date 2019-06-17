612593017eae2474b8df6b879fa3dcf9e3a763f9
Chi Chen <chenchiapply@gmail.com>
2019-06-16 23:17:53 -0400

    [Generator] disable scaffold.scss generation, and set slim as template_engine

--- config/application.rb
+++ config/application.rb
@@ -11,5 +11,9 @@ module MyRailsRepoSkeleton
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
+    config.generators do |g|
+      g.scaffold_stylesheet false
+      g.template_engine :slim
+    end
   end
 end
