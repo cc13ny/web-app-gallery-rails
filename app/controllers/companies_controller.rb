ca45e358571f442c0cfedcc5f96eb4edf409ed64
Chi Chen <cc2015ex@gmail.com>
2019-11-26 01:42:38 -0500

    [Linkedin] Company controller

---
+++ app/controllers/companies_controller.rb
@@ -0,0 +1,49 @@
+class CompaniesController < ApplicationController
+  def index
+    @companies = Company.all
+  end
+
+  def show
+    @company = Company.find(params[:id])
+  end
+
+  def new
+    @company = Company.new
+  end
+
+  def edit
+    @company = Company.find(params[:id])
+  end
+
+  def create
+    @company = Company.new(company_params)
+
+    if @company.save
+      redirect_to @company
+    else
+      render 'new'
+    end
+  end
+
+  def update
+    @company = Company.find(params[:id])
+
+    if @company.update(company_params)
+      redirect_to @company
+    else
+      render 'edit'
+    end
+  end
+
+  def destroy
+    @company = Company.find(params[:id])
+    @company.destroy
+
+    redirect_to companys_path
+  end
+
+  private
+    def company_params
+      params.require(:company).permit(:name, :description)
+    end
+end
