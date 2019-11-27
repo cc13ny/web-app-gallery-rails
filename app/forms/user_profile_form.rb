33f9b8a3ac67e5034d664c23eb4dd7f7feb4b771
Chi Chen <cc2015ex@gmail.com>
2019-11-26 21:35:43 -0500

    [Linkedin] wip - user_profile_form, account_form, rspec for form

--- app/forms/user_profile_form.rb
+++ app/forms/user_profile_form.rb
@@ -1,9 +1,42 @@
 class UserProfileForm < Reform::Form
-  property :email,              on: :user
-  properties :gender, :age,     on: :profile
+  include Reform::Form::ActiveModel::ModelReflections # for cocoon
+  property :first_name
+  property :last_name
+  property :email
 
-  model :user, on: :user
 
-  validates :email, :gender, presence: true
-  validates :age, numericality: true
+
+  property :profile do
+    property :about
+    property :headline
+  end
+
+  collection :experiences do
+    property :employer_name
+    property :company_id
+    property :start_year
+    property :start_month
+    property :end_year
+    property :end_month
+    property :current
+    property :description
+    property :location
+  end
+
+  collection :educations do
+    property :school_name
+    property :degree
+    property :field_of_study
+    property :start_year
+    property :start_month
+    property :end_year
+    property :end_month
+    property :current
+    property :gpa
+    property :description
+  end
+
+  def initialize(user)
+    super(user)
+  end
 end
