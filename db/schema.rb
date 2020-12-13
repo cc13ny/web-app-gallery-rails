a19533f17a0c2a90e8f980672c1889a1dd9df756
Chi Chen <chenchiapply@gmail.com>
2020-12-13 13:53:04 -0500

    Use PostgreSQL instead of MySQL
    - run 'bundle update'
    - Install nvm locally (not via homebrew) and remove therubyracer due to devcenter.heroku.com/articles/rails-asset-pipeline#therubyracer
    - Database
      - Use PostgreSQL instead of MySQL
      - Update the admin, password (via EnvVar instead of Hardcoded), dev and test databases
      - run 'rails db:create'
      - run 'rails db:migrate'

--- db/schema.rb
+++ db/schema.rb
@@ -12,7 +12,10 @@
 
 ActiveRecord::Schema.define(version: 20190615025623) do
 
-  create_table "users", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=latin1" do |t|
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
+  create_table "users", force: :cascade do |t|
     t.datetime "created_at",                          null: false
     t.datetime "updated_at",                          null: false
     t.string   "email",                  default: "", null: false
